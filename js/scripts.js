8bc4698db23c41ce195cbea42df401436b984e26
Markus Thömmes <markusthoemmes@me.com>
2020-03-21 13:02:32 +0100

    Use tensorflow audio recognition example.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-// Our scripts go here.
+let recognizer;
+
+function predictWord() {
+ // Array of words that the recognizer is trained to recognize.
+  const words = recognizer.wordLabels();
+  recognizer.listen(({scores}) => {
+    // Turn scores into a list of (score,word) pairs.
+    scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
+    // Find the most probable word.
+    scores.sort((s1, s2) => s2.score - s1.score);
+    document.querySelector('#prediction').textContent = 'Ich glaube du has "' + scores[0].word + '" gesagt.';
+  }, {probabilityThreshold: 0.75});
+}
+
+async function app() {
+  recognizer = speechCommands.create('BROWSER_FFT');
+  await recognizer.ensureModelLoaded();
+  document.querySelector('#prediction').textContent = 'Modell geladen';
+  predictWord();
+}
+
+app();
